777b9190e0b3a879ba9b4b44adb4bbdfdf36f0b5
Alberto Garcia Guillen <alberto.garciaguillen.ext@telefonica.com>
2020-02-11 12:57:45 +0100

    First commit. Templates and css style.

---
+++ flaskblog.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Link Example',
+        'title': 'Link title',
+        'content': 'link',
+        'date_posted': 'Format description'
+    },
+    {
+        'author': 'Link Example',
+        'title': 'Link title2',
+        'content': 'link2',
+        'date_posted': 'Format description'
+    },
+]
+
+
+@app.route("/")
+@app.route("/hls")
+def hls():
+    return render_template('hls.html', posts=posts, title='HLS')
+
+
+@app.route("/smoothstreaming")
+def smoothstreaming():
+    return render_template('smoothstreaming.html', posts=posts, title='SmoothStreaming')
+
+@app.route("/dash")
+def dash():
+    return render_template('dash.html', posts=posts, title='DASH')
+
+@app.route("/transportstream")
+def transportstream():
+    return render_template('transportstream.html', posts=posts, title='TransportStream')
+
+@app.route("/drm")
+def drm():
+    return render_template('drm.html', posts=posts, title='DRM')
+
+if __name__ == '__main__':
+    app.run(debug=True)
