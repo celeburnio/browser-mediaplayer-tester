40ff1753691347433e0ca0d526dc1ad0d82da2c0
Alberto Garcia Guillen <alberto.garciaguillen.ext@telefonica.com>
2020-02-12 13:31:38 +0100

    Hls examples included

--- app/routes.py
+++ app/routes.py
@@ -16,28 +16,45 @@ posts = [
     },
 ]
 
+postsHls = [
+    {
+        'title': 'Apple Basic Stream 4:3',
+        'link': 'https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8',
+        'format_description': 'Apple Basic Stream: H.264 @ 30Hz, 4x3 aspect ratio'
+    },
+    {
+        'title': 'Apple Basic Stream 16:9',
+        'link': 'https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8',
+        'format_description': 'Apple Basic Stream: 16x9 aspect ratio, H.264 @ 30Hz, single .ts file, with byte-ranges in the playlists'
+    },
+    {
+        'title': 'Apple Advanced Stream',
+        'link': 'https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8',
+        'format_description': 'Apple Advanced Stream fMP4: H.264 and HEVC @ 30Hz and 60Hz, 16x9 aspect ratio'
+    },
+]
 
 @app.route("/")
 @app.route("/hls")
 def hls():
-    return render_template('hls.html', posts=posts, title='HLS')
+    return render_template('testpage.html', posts=postsHls, title='HLS')
 
 
 @app.route("/smoothstreaming")
 def smoothstreaming():
-    return render_template('smoothstreaming.html', posts=posts, title='SmoothStreaming')
+    return render_template('testpage.html', posts=posts, title='SmoothStreaming')
 
 @app.route("/dash")
 def dash():
-    return render_template('dash.html', posts=posts, title='DASH')
+    return render_template('testpage.html', posts=posts, title='DASH')
 
 @app.route("/transportstream")
 def transportstream():
-    return render_template('transportstream.html', posts=posts, title='TransportStream')
+    return render_template('testpage.html', posts=posts, title='TransportStream')
 
 @app.route("/drm")
 def drm():
-    return render_template('drm.html', posts=posts, title='DRM')
+    return render_template('testpage.html', posts=posts, title='DRM')
 
 if __name__ == '__main__':
     app.run(debug=True)
