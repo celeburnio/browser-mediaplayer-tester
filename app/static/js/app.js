e95ff771086a4d3503d118db171108a47f9001d0
Alberto Garcia Guillen <alberto.garciaguillen.ext@telefonica.com>
2020-02-13 12:17:38 +0100

    Adding keyboard keys support to move focus

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,4 +1,60 @@
-document.onkeypress = function (e) {
+document.onkeypress = checkKey;
+document.onkeydown = checkKey;
+
+function checkKey (e) {
     e = e || window.event;
-    console.log(e.code)
+
+    if (e.keyCode === 40) { //arrowDown
+
+    }
+    if (e.keyCode === 38) { //arrowUp
+
+    }
+    if (e.keyCode === 39) { //arrowRight
+        if (!tabFocused(document.activeElement.id)) {
+            console.log("set focus to hls");
+            document.getElementById("hls").focus();
+        } else if (document.activeElement.id === "hls") {
+            document.getElementById("smoothstreaming").focus();
+        } else if (document.activeElement.id === "smoothstreaming") {
+            document.getElementById("dash").focus();
+        } else if (document.activeElement.id === "dash") {
+            document.getElementById("transportstream").focus();
+        } else if (document.activeElement.id === "transportstream") {
+            document.getElementById("drm").focus();
+        } else if (document.activeElement.id === "drm") {
+            document.getElementById("hls").focus();
+        }
+    }
+    if (e.keyCode === 37) { //arrowLeft
+        if (!tabFocused(document.activeElement.id)) {
+            document.getElementById("hls").focus();
+        } else if (document.activeElement.id === "hls") {
+            document.getElementById("drm").focus();
+        } else if (document.activeElement.id === "drm") {
+            document.getElementById("transportstream").focus();
+        } else if (document.activeElement.id === "transportstream") {
+            document.getElementById("dash").focus();
+        } else if (document.activeElement.id === "dash") {
+            document.getElementById("smoothstreaming").focus();
+        } else if (document.activeElement.id === "smoothstreaming") {
+            document.getElementById("hls").focus();
+        }
+    }
 };
+
+function tabFocused(id) {
+    let ret = false;
+    switch (id) {
+        case "hls":
+        case "smoothstreaming":
+        case "dash":
+        case "transportstream":
+        case "drm":
+            ret = true;
+            break;
+        default:
+            break;
+    }
+    return ret;
+}
